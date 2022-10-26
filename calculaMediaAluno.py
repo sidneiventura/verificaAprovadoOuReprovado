8b155b1040f5fb9c62a442907aea27f1c594e9e9
sidneiventura <82735294+sidneiventura@users.noreply.github.com>
2022-10-26 19:05:40 -0300

    Add files via upload

---
+++ calculaMediaAluno.py
@@ -0,0 +1,12 @@
+aluno = "Paulo"
+nota1 = 10
+nota2 = 8
+media = (nota1 + nota2) / 2
+faltas = 4
+
+if media < 7:
+  print ("Que pena", aluno, ", você foi reprovado")
+elif faltas > 3:
+  print ("Que pena", aluno, ", você foi reprovado")
+else:
+  print ("Parabéns", aluno , ", você foi aprovado")
